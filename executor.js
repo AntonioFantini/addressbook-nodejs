dc605756b47a58cb3fc60c251084470c2b8554de
Antonio Fantini <antonio.fantini88@gmail.com>
2015-06-04 16:56:47 +0200

    second commit

--- executor.js
+++ executor.js
@@ -1,12 +1,11 @@
 var fs = require('fs');
 var personFactory = require('./person.js');
-global.operationOldest='OLDEST',
-
+global.operationOldest='OLDEST';
 module.exports = {
 	execute: function(operation){
 		var people = JSON.parse(fs.readFileSync('AddressBook.json','utf8'));
-		if(operation == operationOldest){
-			console.log(operationOldest);
+		if(operation == global.operationOldest){
+			console.log(global.operationOldest);
 		}else{
 			for (var i=0; i< people.length; i++){
 				var person = personFactory.createPerson(people[i].name, people[i].sex, people[i].dob);
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 var executor = require('./executor.js');
-var utils = require('./utils/addressbook_utils')
+var utils = require('./utils/addressbook_utils');
 var arg = process.argv[2];
-console.log('arg: '+ arg);
 if(utils.checkEmpty(arg)){
-	executor.execute();
-}else if(arg == '0'){
-	executor.execute(executor.operationOldest);
+	executor.execute('');
+}else if(+arg == 0){
+	executor.execute(global.operationOldest);
 }
